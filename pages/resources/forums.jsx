6827c3781d74af43a4d130e3affd1c75bb10298e
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2023-04-23 15:43:52 +0300

    another resources pages

---
+++ pages/resources/forums.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material"
+
+const Forums = () => {
+  return (
+    <Typography>Forums Page</Typography>
+  )
+}
+
+export default Forums
---
+++ pages/resources/tutorials.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material"
+
+const Tutorials = () => {
+  return (
+    <Typography>Tutorials Page</Typography>
+  )
+}
+
+export default Tutorials
