b809302a22e67f90e9c30be716f57cd09edb315e
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2023-07-27 11:15:37 +0300

    unused components: incase needed

---
+++ components/unused/FAQ.jsx
@@ -0,0 +1,96 @@
+import { Typography } from '@mui/material';
+
+export function FAQ() {
+  return (
+    <>
+      <div style={{ display: 'flex', width: '98.5vw' }}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '33.3vw',
+            padding: '2rem',
+            gap: '.5rem',
+          }}
+        >
+          <Typography variant="h5">Does TaxPal handle VAT?</Typography>
+          <Typography>
+            Well no, but if you move your company offshore you can probably ignore it.
+          </Typography>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            width: '33.3vw',
+            flexDirection: 'column',
+            padding: '2rem',
+            gap: '.5rem',
+          }}
+        >
+          <Typography variant="h5">Does TaxPal handle VAT?</Typography>
+          <Typography>
+            Well no, but if you move your company offshore you can probably ignore it.
+          </Typography>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            width: '33.3vw',
+            flexDirection: 'column',
+            padding: '2rem',
+            gap: '.5rem',
+          }}
+        >
+          <Typography variant="h5">Does TaxPal handle VAT?</Typography>
+          <Typography>
+            Well no, but if you move your company offshore you can probably ignore it.
+          </Typography>
+        </div>
+      </div>
+      <div style={{ display: 'flex', width: '98.5vw' }}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '33.3vw',
+            padding: '2rem',
+            gap: '.5rem',
+          }}
+        >
+          <Typography variant="h5">Does TaxPal handle VAT?</Typography>
+          <Typography>
+            Well no, but if you move your company offshore you can probably ignore it.
+          </Typography>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            width: '33.3vw',
+            flexDirection: 'column',
+            padding: '2rem',
+            gap: '.5rem',
+          }}
+        >
+          <Typography variant="h5">Does TaxPal handle VAT?</Typography>
+          <Typography>
+            Well no, but if you move your company offshore you can probably ignore it.
+          </Typography>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            width: '33.3vw',
+            flexDirection: 'column',
+            padding: '2rem',
+            gap: '.5rem',
+          }}
+        >
+          <Typography variant="h5">Does TaxPal handle VAT?</Typography>
+          <Typography>
+            Well no, but if you move your company offshore you can probably ignore it.
+          </Typography>
+        </div>
+      </div>
+    </>
+  );
+}
