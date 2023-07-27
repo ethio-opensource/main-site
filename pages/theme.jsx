04aa2e6b5a786c02cb1c6a0268b36a6e3566443d
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2023-07-27 11:14:38 +0300

    just reformatted

--- pages/theme.jsx
+++ pages/theme.jsx
@@ -1,141 +1,132 @@
-import { createContext,useState,useMemo } from "react";
-import { createTheme } from "@mui/material/styles";
-
+import { createContext, useState, useMemo } from 'react';
+import { createTheme } from '@mui/material/styles';
 
 // Color designs
-export const tokens = (mode) =>({
-    ...(mode === 'light' ? {
-
-        primary:{
-            600:'#1315BD',
-            500:'#373AEB',
-            400:'#7173F1',
-            300:'#AEAFF7',
-            200:'#D7D7FB',
-            100:'#EBEBFD',
+export const tokens = (mode) => ({
+  ...(mode === 'light'
+    ? {
+        primary: {
+          600: '#1315BD',
+          500: '#373AEB',
+          400: '#7173F1',
+          300: '#AEAFF7',
+          200: '#D7D7FB',
+          100: '#EBEBFD',
         },
-        secondary:{
-            600:'#3F4957',
-            500:'#57667A',
-            400:'#8996AA',
-            300:'#B0B9C6',
-            200:'#D8DCE3',
-            100:'#EBEEF1',
+        secondary: {
+          600: '#3F4957',
+          500: '#57667A',
+          400: '#8996AA',
+          300: '#B0B9C6',
+          200: '#D8DCE3',
+          100: '#EBEEF1',
         },
-            
-        neutral:{
-            400:'#FFFF',
-            700:'#0F172A',
-        
-        }
-
-    }: {
-
-        primary:{
-            600:'#1315BD',
-            500:'#373AEB',
-            400:'#7173F1',
-            300:'#AEAFF7',
-            200:'#D7D7FB',
-            100:'#EBEBFD',
+        neutral: {
+          400: '#FFFF',
+          700: '#0F172A',
         },
-        secondary:{
-            600:'#3F4957',
-            500:'#57667A',
-            400:'#8996AA',
-            300:'#B0B9C6',
-            200:'#D8DCE3',
-            100:'#EBEEF1',
-        
+      }
+    : {
+        primary: {
+          600: '#1315BD',
+          500: '#373AEB',
+          400: '#7173F1',
+          300: '#AEAFF7',
+          200: '#D7D7FB',
+          100: '#EBEBFD',
         },
-        neutral:{
-            400:'#FFFF',
-            700:'#0F172A',
-        
-        }
-
-    } )
-})
+        secondary: {
+          600: '#3F4957',
+          500: '#57667A',
+          400: '#8996AA',
+          300: '#B0B9C6',
+          200: '#D8DCE3',
+          100: '#EBEEF1',
+        },
+        neutral: {
+          400: '#FFFF',
+          700: '#0F172A',
+        },
+      }),
+});
 
 //material ui theme setting
 
-export const themeSettings = (mode) =>{
-    const colors = tokens(mode)
+export const themeSettings = (mode) => {
+  const colors = tokens(mode);
 
-    return {
-        palatte:{
-            mode:mode,
-            ...(mode === 'light' ?
-            {
-                primary:{
-                    main: colors.primary[500],
-                },
-                secondary:{
-                    main: colors.secondary[500],
-                },
-                neutral:{
-                    main: colors.neutral[400],
-                    dark: colors.neutral[700]
-                }
-            }:{
-                primary:{
-                    main: colors.primary[600]
-                },
-                secondary:{
-                    main: colors.secondary[600]
-                },
-                neutral:{
-                    main: colors.neutral[700],
-                },
-                background:{
-                    default: colors.primary[600]
-                }
-
-            }),
-            typography:{
-                fontFamily:['Work Sans','Lexend'].join(","),
-                fontSize: 13,
-                h1:{
-                    fontFamily:['Lexend','Work Sans'].join(","),
-                    fontSize: 34,
-                },
-                h2:{
-                    fontFamily:['Lexend','Work Sans'].join(","),
-                    fontSize: 20,
-                },
-                h3:{
-                    fontFamily:['Lexend','Work Sans'].join(","),
-                    fontSize: 13,
-                },
-                h4:{
-                    fontFamily:['Work Sans','Lexend'].join(","),
-                    fontSize: 18,
-                },
-                h5:{
-                    fontFamily:['Work Sans','Lexend'].join(","),
-                    fontSize: 16,
-                },
-            }
-        }
-    }
-}
-
+  return {
+    palatte: {
+      mode: mode,
+      ...(mode === 'light'
+        ? {
+            primary: {
+              main: colors.primary[500],
+            },
+            secondary: {
+              main: colors.secondary[500],
+            },
+            neutral: {
+              main: colors.neutral[400],
+              dark: colors.neutral[700],
+            },
+          }
+        : {
+            primary: {
+              main: colors.primary[600],
+            },
+            secondary: {
+              main: colors.secondary[600],
+            },
+            neutral: {
+              main: colors.neutral[700],
+            },
+            background: {
+              default: colors.primary[600],
+            },
+          }),
+      typography: {
+        fontFamily: ['Work Sans', 'Lexend'].join(','),
+        fontSize: 13,
+        h1: {
+          fontFamily: ['Lexend', 'Work Sans'].join(','),
+          fontSize: 34,
+        },
+        h2: {
+          fontFamily: ['Lexend', 'Work Sans'].join(','),
+          fontSize: 20,
+        },
+        h3: {
+          fontFamily: ['Lexend', 'Work Sans'].join(','),
+          fontSize: 13,
+        },
+        h4: {
+          fontFamily: ['Work Sans', 'Lexend'].join(','),
+          fontSize: 18,
+        },
+        h5: {
+          fontFamily: ['Work Sans', 'Lexend'].join(','),
+          fontSize: 16,
+        },
+      },
+    },
+  };
+};
 
 export const colorModeContext = createContext({
-    toggleColorMode: () =>{}
-})
+  toggleColorMode: () => {},
+});
 
-export const useMode = () =>{
-    const [mode, setMode] = useState("light")
+export const useMode = () => {
+  const [mode, setMode] = useState('light');
 
-    const colorMode = useMemo(
-        ()=>({
-        toggleColorMode:() =>{
-            setMode((prev) => (prev === 'light' ? 'dark' :'light'))
-        }
-    }))
+  const colorMode = useMemo(() => ({
+    toggleColorMode: () => {
+      setMode((prev) => (prev === 'light' ? 'dark' : 'light'));
+    },
+  }));
 
-    const theme = useMemo(() =>createTheme(themeSettings(mode),[mode]))
+  const theme = useMemo(() => createTheme(themeSettings(mode), [mode]));
 
-    return [theme, colorMode]
-}
+  return [theme, colorMode];
+};
