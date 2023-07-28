720b017761f97966b83c43dbd4907c2b9990c239
Okitta Ongaye <Okitta9388@gmail.com>
2023-07-28 17:22:41 +0300

    start project detail page

--- components/Project.jsx
+++ components/Project.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Card from '@mui/joy/Card';
 import CardCover from '@mui/joy/CardCover';
 import CardContent from '@mui/joy/CardContent';
+import Link from '@mui/joy/Link';
 import Typography from '@mui/joy/Typography';
 import LocationOnRoundedIcon from '@mui/icons-material/LocationOnRounded';
 
@@ -10,29 +11,31 @@ export default function Project() {
     'https://images.unsplash.com/photo-1542773998-9325f0a098d7?auto=format&fit=crop&w=320';
 
   return (
-    <Card sx={{ minHeight: '280px', width: 320 }}>
-      <CardCover>
-        <img
-          src="https://images.unsplash.com/photo-1542773998-9325f0a098d7?auto=format&fit=crop&w=320"
-          srcSet="https://images.unsplash.com/photo-1542773998-9325f0a098d7?auto=format&fit=crop&w=320&dpr=2 2x"
-          loading="lazy"
-          alt=""
+    <Link href="projectDetail" underline='none'>    
+      <Card sx={{ minHeight: '280px', width: 320,}}>
+        <CardCover>
+          <img
+            src="https://images.unsplash.com/photo-1542773998-9325f0a098d7?auto=format&fit=crop&w=320"
+            srcSet="https://images.unsplash.com/photo-1542773998-9325f0a098d7?auto=format&fit=crop&w=320&dpr=2 2x"
+            loading="lazy"
+            alt=""
+          />
+        </CardCover>
+        <CardCover
+          sx={{
+            background:
+              'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)',
+          }}
         />
-      </CardCover>
-      <CardCover
-        sx={{
-          background:
-            'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)',
-        }}
-      />
-      <CardContent sx={{ justifyContent: 'flex-end' }}>
-        <Typography level="h2" fontSize="lg" textColor="#fff" mb={1}>
-          Yosemite National Park
-        </Typography>
-        <Typography startDecorator={<LocationOnRoundedIcon />} textColor="neutral.300">
-          California, USA
-        </Typography>
-      </CardContent>
-    </Card>
+        <CardContent sx={{ justifyContent: 'flex-end' }}>
+          <Typography level="h2" fontSize="lg" textColor="#fff" mb={1}>
+            Yosemite National Park
+          </Typography>
+          <Typography startDecorator={<LocationOnRoundedIcon />} textColor="neutral.300">
+            California, USA
+          </Typography>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
---
+++ pages/projectDetail.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Button, Link, Typography } from '@mui/material';
+
+const ProjectDetail = () => {
+  return (
+    <div style={{ display: 'flex', backgroundColor: 'white' }}>
+      <div style={{ flex: 1, padding: '20px' }}>
+        <img
+          src="/images/ceo.jpg" // Replace with the URL of your project image
+          alt="Project Image"
+          style={{ width: '75%', borderRadius: '8px' }}
+        />
+        <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'flex-start', gap:'1rem'}}>
+        <Button
+          variant="contained"
+          sx={{
+            marginTop: '10px',
+            borderRadius: '20px',
+            backgroundColor: '#B0FFE3',
+            color: '#2DC133',
+            '&:hover': {
+              backgroundColor: '#B0FFE3',
+            },
+          }}
+        >
+          Deployed
+        </Button>
+        <Link href="https://github.com" underline="none" style={{color:'black'}} sx={{ display: 'block', marginBottom: '10px' }}>
+          GitHub Link
+        </Link>
+        <Link href="https://docs.google.com/presentation" style={{color:'black'}} underline="none" sx={{ display: 'block', marginBottom: '10px' }}>
+          Google Slide Link
+        </Link>
+        <Link href="https://t.me/topic" underline="none" style={{color:'black'}} sx={{ display: 'block', marginBottom: '10px' }}>
+          Telegram Topic Link
+        </Link>
+      </div>
+        </div>
+      <div style={{ flex: 1, padding: '20px', }}>
+        <Typography variant="h2" sx={{ fontSize: '36px',fontWeight:'700',marginBottom:'20px' }}>
+          Project Title
+        </Typography>
+        <Typography variant="body1" sx={{ fontSize: '16px' }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam officia, fugit obcaecati culpa, laborum modi temporibus quis quaerat, eos mollitia beatae impedit delectus commodi possimus laudantium aliquam eaque tempora tenetur distinctio illum accusantium. Enim repellat sunt, amet dolorum provident blanditiis similique unde, tempore quaerat maiores eveniet perspiciatis! Officia labore, aliquam, rerum aspernatur quo voluptatum dignissimos velit temporibus ea minus in at et laborum sequi? Eligendi sequi impedit nam repellat blanditiis. Animi ratione possimus temporibus saepe omnis repellat harum adipisci, expedita voluptas earum ut! Facere, aperiam alias facilis inventore consequuntur odio nihil a deleniti ab, nemo, dignissimos accusantium iusto blanditiis dolorum!
+        </Typography>
+        <Typography variant="h3" sx={{ fontSize: '36px',fontWeight:'700',marginY:'20px' }}>
+          Readme
+        </Typography>
+        <Typography variant="body1" sx={{ fontSize: '16px' }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, dignissimos reprehenderit. Rem eius praesentium expedita aliquam, quas est fugit dolores magnam autem non, omnis obcaecati, cumque voluptatum magni dignissimos at. Soluta quod possimus molestiae, deleniti nostrum recusandae temporibus facere ducimus totam. Repudiandae sit possimus dolores nihil corrupti qui, deserunt quasi animi doloremque vitae sequi labore deleniti quos sunt ex excepturi doloribus quo tempora quas maxime veniam voluptatum impedit. Sed doloremque unde maxime, dolores eaque voluptatibus? Ipsam sapiente dolores molestias eligendi saepe molestiae dicta delectus expedita dolorum veritatis ex quae blanditiis, voluptates a nam quibusdam quasi! Officia ducimus obcaecati quod repellat.
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetail;
