701bb7099b0ecd3f216692b4bb89692d74143f2f
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2023-07-30 21:25:26 +0300

    minor name change

--- store/assets.js
+++ store/assets.js
@@ -1,5 +1,5 @@
 export const assets = {
     img1: 'https://ucarecdn.com/1378c24d-df86-48e7-a1eb-ad36585ca1d8/docs1.png',
     img2: 'https://ucarecdn.com/9f9fce60-57bb-4927-bd27-3ab4344d5e65/docs2png.png',
-    eosLogo: 'https://ucarecdn.com/00c0c004-7023-4298-8a18-66c778fc2ffd/eoslogotransparentblack250.png'
+    logo: 'https://ucarecdn.com/00c0c004-7023-4298-8a18-66c778fc2ffd/eoslogotransparentblack250.png'
 }
